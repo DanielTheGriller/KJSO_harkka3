c461d0736620c2e3c0abdee9bbaef70964eaea8e
DanielTheGriller <daniel.linna@gmail.com>
2018-12-16 16:30:34 +0200

    first commit

---
+++ getreadcount.c
@@ -0,0 +1,17 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
+
+// Funtion to call getreadcount from xv6 terminal
+int
+main(int argc, char *argv[]){
+// If any argument is passed, call resetreadcount
+	if (argc > 1){
+		resetreadcount();
+	} else {
+// If no arguments, call getreadcount
+		getreadcount();
+	}
+	exit();
+}
